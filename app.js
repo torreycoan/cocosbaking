afbce34e3f630c185aaf3d8225a9e70a5b39f3a7
Trent <jarvi@wisc.edu>
2023-10-12 13:33:35 -0500

    sign in modal work

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 let signuplink = document.querySelector("#signuplink");
 let signupmodalbg = document.querySelector("#signupmodalbg");
 let signupmodal = document.querySelector("#signupmodal");
-function showmodal() {
+function showsignupmodal() {
   signupmodal.classList.add("is-active");
 }
-function hidemodal() {
+function hidesignupmodal() {
   signupmodal.classList.remove("is-active");
 }
 
-signuplink.addEventListener("click", showmodal);
+signuplink.addEventListener("click", showsignupmodal);
 
-signupmodalbg.addEventListener("click", hidemodal);
+signupmodalbg.addEventListener("click", hidesignupmodal);
+
+// sign in environment
+let signinlink = document.querySelector("#signinlink");
+let signinmodalbg = document.querySelector("#signinmodalbg");
+let signinmodal = document.querySelector("#signinmodal");
+function showsigninmodal() {
+  signinmodal.classList.add("is-active");
+}
+function hidesigninmodal() {
+  signinmodal.classList.remove("is-active");
+}
+
+signinlink.addEventListener("click", showsigninmodal);
+
+signinmodalbg.addEventListener("click", hidesigninmodal);
