e9ef9254defa768a7e01e415aaf16153ab814265
Trent <jarvi@wisc.edu>
2023-10-20 17:07:54 -0500

    working on authentication

--- public/app.js
+++ public/app.js
@@ -16,6 +16,10 @@ function hidemodal(modal) {
 
 // ------------------------------------------------------------
 
+// messages
+
+// ------------------------------------------------------------
+
 // sign up and sign in modals
 // sign up modal
 
@@ -41,34 +45,116 @@ query("signinmodalbg").addEventListener("click", () => {
 // showing and hiding parts of main (home, products, order now)
 
 query("home").addEventListener("click", () => {
-  query("homapage").remove("is-hidden");
-  query("homapage").add("is-active");
+  query("homepage").classList.remove("is-hidden");
+  query("homepage").classList.add("is-active");
 
-  query("productpage").remove("is-active");
-  query("productpage").add("is-hidden");
+  query("productpage").classList.remove("is-active");
+  query("productpage").classList.add("is-hidden");
 
-  query("orderpage").remove("is-active");
-  query("orderpage").add("is-hidden");
+  query("orderpage").classList.remove("is-active");
+  query("orderpage").classList.add("is-hidden");
 });
 
 query("products").addEventListener("click", () => {
-  query("productpage").remove("is-hidden");
-  query("productpage").add("is-active");
+  query("productpage").classList.remove("is-hidden");
+  query("productpage").classList.add("is-active");
 
-  query("homepage").remove("is-active");
-  query("homepage").add("is-hidden");
+  query("homepage").classList.remove("is-active");
+  query("homepage").classList.add("is-hidden");
 
-  query("orderpage").remove("is-active");
-  query("orderpage").add("is-hidden");
+  query("orderpage").classList.remove("is-active");
+  query("orderpage").classList.add("is-hidden");
 });
 
 query("orders").addEventListener("click", () => {
-  query("orderpage").add("is-active");
-  query("orderpage").remove("is-hidden");
+  query("orderpage").classList.remove("is-hidden");
+  query("orderpage").classList.add("is-active");
 
-  query("homepage").remove("is-active");
-  query("homepage").add("is-hidden");
+  query("productpage").classList.remove("is-active");
+  query("productpage").classList.add("is-hidden");
 
-  query("productpage").remove("is-active");
-  query("productpage").add("is-hidden");
+  query("homepage").classList.remove("is-active");
+  query("homepage").classList.add("is-hidden");
+});
+
+// ------------------------------------------------------------
+
+// resetting sign up and sign in fields
+
+query("resetsignupfields").addEventListener("click", (e) => {
+  e.preventDefault();
+  query("signup").reset();
+});
+
+query("resetsigninfields").addEventListener("click", (e) => {
+  e.preventDefault();
+  query("signin").reset();
+});
+
+// ------------------------------------------------------------
+
+// create a user
+
+query("signupbtn").addEventListener("click", (e) => {
+  alert("hello");
+  // e.preventDefault();
+
+  // // email and password
+
+  // let email = query("signupemail").value;
+  // let password = query("signuppassword").value;
+
+  // auth.createUserWithEmailAndPassword(email, password).then((user) => {
+  //   query("signup").reset();
+  //   query("signupmodalcontent").classList.add("is-hidden");
+
+  //   query(
+  //     "signupmessage"
+  //   ).innerHTML = `You (${user.email}) have succussfully signed up!`;
+
+  //   query("signupmessage").classList.remove("is-hidden");
+
+  //   setTimeout(() => {
+  //     query("signupmessage").classList.add("is-hidden");
+  //     query("signupmessage").innerHTML = ``;
+  //     query(
+  //       "signupmodalcontent"
+  //     ).innerHTML = `<h2 class="subtitle is-size-3 has-text-weight-bold has-text-centered">
+  //     WELCOME TO COCO'S BAKING!
+  //   </h2>
+  //   <!-- sign up form -->
+  //   <form id="signup">
+  //     <!-- Name -->
+  //     <div class="field">
+  //       <label class="label">Name</label>
+  //       <div class="control">
+  //         <input class="input" type="text" id="email" />
+  //       </div>
+  //     </div>
+  //     <!-- email -->
+  //     <div class="field">
+  //       <label class="label">Email</label>
+  //       <div class="control">
+  //         <input class="input" type="text" id="email" />
+  //       </div>
+  //     </div>
+
+  //     <!-- password -->
+  //     <div class="field">
+  //       <label class="label">Password</label>
+  //       <div class="control">
+  //         <input type="password" class="input" id="password" />
+  //       </div>
+  //     </div>
+  //     <div class="has-text-centered">
+  //       <button class="button mt-5 mr-3" id="signupbtn">Sign Up</button>
+  //       <button class="button has-text-danger mt-5" id="resetsignupfields">
+  //         Reset Fields
+  //       </button>
+  //     </div>
+  //   </form>`;
+  //     query("signupmodalcontent").classList.remove("is-hidden");
+  //     query("signupmodalcontent").classList.add("is-active");
+  //   }, 3000);
+  // });
 });
