f7a8c5b4aaccaccb0f170baf09eaa5b11aba084e
Torrey Coan <tcoan21@gmail.com>
2023-12-07 20:46:17 -0600

    added testing file

---
+++ public/testing.js
@@ -0,0 +1,63 @@
+// import puppeteer
+
+const puppeteer = require("puppeteer");
+
+async function go() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 15,
+
+    });
+
+    const page = await browser.newPage();
+
+    //   site the to be tested
+
+    await page.goto("https://cocobaking-88eae.web.app/");
+
+    await page.click("#burger_nav");
+    //   user click the sign-in button
+    await new Promise((r) => setTimeout(r, 2000));
+    await page.click("#signinlink");
+
+    // user will provide email, password, and interests information
+
+    await page.type("#signinemail", "twaste210@gmail.com");
+    await page.type("#signinpassword", "easypassword");
+
+    await page.click("#signinbtn");
+
+    //   test the search functionality
+
+    //   set 2 second delay
+
+    await new Promise((r) => setTimeout(r, 2000));
+    await page.click("#burger_nav");
+    await page.click("#orders");
+    await page.type("#firstname", "Testing");
+    await page.type("#lastname", "File");
+
+    await page.click("#productselection");
+    await page.keyboard.press('B') // select blueberry
+    await page.keyboard.press('Enter')
+
+    await page.keyboard.press('Tab')
+    await page.keyboard.press('Tab')
+    await page.keyboard.press('Tab')
+    await page.keyboard.press('Tab')
+    //await page.keyboard.press('Tab')
+    await page.type("#completiondate", "11112024");
+
+    await page.click("#orderbutton")
+
+
+    await page.click("#burger_nav");
+    await page.click("#signoutlink");
+
+
+
+}
+
+// call the go()
+
+go();
