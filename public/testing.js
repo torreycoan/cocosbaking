ffc5fcd3e80e211f761edf1c50a62714a7a0054e
Trent <jarvi@wisc.edu>
2023-12-11 16:29:23 -0600

    getting testing file to work

--- public/testing.js
+++ public/testing.js
@@ -14,41 +14,41 @@ async function go() {
 
   await page.goto("https://cocobaking-88eae.web.app/");
 
-  await page.click("#burger_nav");
-  //   user click the sign-in button
-  await new Promise((r) => setTimeout(r, 2000));
-  await page.click("#signinlink");
+  // await page.click("#burger_nav");
+  // //   user click the sign-in button
+  // await new Promise((r) => setTimeout(r, 2000));
+  // await page.click("#signinlink");
 
-  // user will provide email, password, and interests information
+  // // user will provide email, password, and interests information
 
-  await page.type("#signinemail", "twaste210@gmail.com");
-  await page.type("#signinpassword", "easypassword");
+  // await page.type("#signinemail", "twaste210@gmail.com");
+  // await page.type("#signinpassword", "easypassword");
 
-  await page.click("#signinbtn");
+  // await page.click("#signinbtn");
 
-  //   test the search functionality
+  // //   test the search functionality
 
-  //   set 2 second delay
+  // //   set 2 second delay
 
-  await new Promise((r) => setTimeout(r, 2000));
-  await page.click("#burger_nav");
-  await page.click("#orders");
+  // await new Promise((r) => setTimeout(r, 2000));
+  // await page.click("#burger_nav");
+  // await page.click("#orders");
 
-  await page.click("#productselection");
-  await page.keyboard.press("B"); // select blueberry
-  await page.keyboard.press("Enter");
+  // await page.click("#productselection");
+  // await page.keyboard.press("B"); // select blueberry
+  // await page.keyboard.press("Enter");
 
-  await page.keyboard.press("Tab");
-  await page.keyboard.press("Tab");
-  await page.keyboard.press("Tab");
-  await page.keyboard.press("Tab");
-  //await page.keyboard.press('Tab')
-  await page.type("#completiondate", "11112024");
+  // await page.keyboard.press("Tab");
+  // await page.keyboard.press("Tab");
+  // await page.keyboard.press("Tab");
+  // await page.keyboard.press("Tab");
+  // //await page.keyboard.press('Tab')
+  // await page.type("#completiondate", "11112024");
 
-  await page.click("#orderbutton");
+  // await page.click("#orderbutton");
 
-  await page.click("#burger_nav");
-  await page.click("#signoutlink");
+  // await page.click("#burger_nav");
+  // await page.click("#signoutlink");
 
   // now testing owner log in
   await page.click("#burger_nav");
@@ -67,36 +67,41 @@ async function go() {
   // gets it to accepted orders
   await page.keyboard.press("A");
   await page.keyboard.press("Enter");
-  await new Promise((r) => setTimeout(r, 2000));
-  await page.click("#filterbutton");
+  await page.keyboard.press("Tab");
+  await page.keyboard.press("Tab");
+  await page.keyboard.press("Enter");
   await new Promise((r) => setTimeout(r, 2000));
   await page.click("#resetfilterbutton");
+  await new Promise((r) => setTimeout(r, 2000));
 
-  //   // then, updating a payment status to paid
-  //   await page.click("#orderu1h3EHkYyKH2UpIGRh6Lnewpaymentstatus");
-  //   await page.keyboard.press("P");
-  //   await page.keyboard.press("Enter");
-  //   await page.click("#u1h3EHkYyKH2UpIGRh6L > td:nth-child(14) > button");
-
+  // then, updating a payment status to paid
+  await page.click("#orderlJztPNZFQy9RszyrHGX8newpaymentstatus");
+  await page.keyboard.press("P");
+  await page.keyboard.press("Enter");
+  await page.click("#lJztPNZFQy9RszyrHGX8 > td:nth-child(14) > button");
   //   // now filter to see if the update went correctly (should only show a one or two orders)
-  //   await page.click("#orderstatusfilter");
-  //   await page.keyboard.press("P");
-  //   await page.keyboard.press("Enter");
-  //   await page.click("#paymentstatusfilter");
-  //   await page.keyboard.press("P");
-  //   await page.keyboard.press("Enter");
-  //   await page.click("#filterbutton");
-  //   await new Promise((r) => setTimeout(r, 2000));
-  //   await page.click("#resetfilterbutton");
+  await page.click("#orderstatusfilter");
+  // gets it to pending acceptance/rejection
+  await page.keyboard.press("P");
+  await page.keyboard.press("Enter");
+  await page.keyboard.press("Tab");
+  // gets it to paid
+  await page.keyboard.press("P");
+  await page.keyboard.press("Tab");
+  await page.keyboard.press("Enter");
+  await new Promise((r) => setTimeout(r, 2000));
+  await page.click("#resetfilterbutton");
+  await new Promise((r) => setTimeout(r, 2000));
 
-  //   // finally, deleting an order
-  //   await page.click("#oQGnMdlnJBqflE8UvjTw > td:nth-child(15) > button");
-  //   await page.click("#cancelorderbtn");
-  //   await new Promise((r) => setTimeout(r, 2000));
+  // finally, deleting an order
+  await page.click("#EqOyPgJNLm0XwhBapchf > td:nth-child(15) > button");
+  await page.click("#cancelorderbtn");
+  await new Promise((r) => setTimeout(r, 2000));
 
   // owner now signs out
   await page.click("#burger_nav");
   await page.click("#signoutlink");
+  await new Promise((r) => setTimeout(r, 2000));
 
   browser.close();
 }
